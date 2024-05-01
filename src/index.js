4d602bd4449bd0ee30538ca3d32e8177bb606947
Asier <asierherrera97@gmail.com>
2024-05-01 20:08:40 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import router from "./routes/router.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use("/api",router);
+
+app.listen(process.env.APP_PORT,()=>{
+    console.log("Servidor en marcha en el puerto "+process.env.APP_PORT);
+})
